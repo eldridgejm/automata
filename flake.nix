893aba47943fa9668ff1ae7538b1dd5d2190c9d5
Justin Eldridge <eldridgejm@gmail.com>
2021-09-16 12:03:40 -0700

    Added dictconfig dependency.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/20.03;
 
-  outputs = { self, nixpkgs }: 
+  inputs.dictconfig.url = github:eldridgejm/dictconfig/master;
+  inputs.dictconfig.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, dictconfig }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
@@ -15,7 +18,12 @@
             python3Packages.buildPythonPackage {
               name = "automata";
               src = ./.;
-              propagatedBuildInputs = with python3Packages; [ pyyaml cerberus jinja2 ];
+              propagatedBuildInputs = with python3Packages; [ 
+                pyyaml
+                cerberus
+                jinja2
+                dictconfig.outputs.defaultPackage.${system}
+              ];
               nativeBuildInputs = with python3Packages; [ pytest black ipython sphinx sphinx_rtd_theme ];
             }
 
