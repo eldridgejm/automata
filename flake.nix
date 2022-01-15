8766ca9076b5280d410bd23703cbc82898bd3a2c
Justin Eldridge <eldridgejm@gmail.com>
2022-01-14 17:12:39 -0800

    Trying apple silicon...

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { self, nixpkgs, dictconfig }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
       {
