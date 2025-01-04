c00b6620542e153e842ab491b733b986dd15ff47
Justin Eldridge <eldridgejm@gmail.com>
2025-01-04 10:13:28 -0500

    Initialized project reorganization.

--- flake.nix
+++ flake.nix
@@ -1,38 +1,64 @@
 {
-  description = "Python package for automating the publication of course materials.";
+  description = "Automatically generate course webpages from annotated materials.";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/21.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
 
   inputs.dictconfig.url = github:eldridgejm/dictconfig/master;
   inputs.dictconfig.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, dictconfig }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-      {
-        automata = forAllSystems (system:
-          with import nixpkgs { system = "${system}"; };
+  outputs = {
+    self,
+    nixpkgs,
+    dictconfig,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+  in rec {
+    automata = forAllSystems (
+      system:
+        with import nixpkgs {system = "${system}";};
+          python3Packages.buildPythonPackage {
+            name = "automata";
+            src = ./.;
+            format = "pyproject";
+            propagatedBuildInputs = with python3Packages; [
+              pyyaml
+              markdown
+              jinja2
+              dictconfig.outputs.defaultPackage.${system}
+            ];
+            doCheck = false;
+          }
+    );
 
-            python3Packages.buildPythonPackage {
-              name = "automata";
-              src = ./.;
-              propagatedBuildInputs = with python3Packages; [ 
-                pyyaml
-                markdown
-                jinja2
-                dictconfig.outputs.defaultPackage.${system}
-              ];
-              nativeBuildInputs = with python3Packages; [ pytest black ipython sphinx sphinx_rtd_theme lxml ];
-              doCheck = false;
-            }
+    devShell = forAllSystems (
+      system:
+        with import nixpkgs {
+          system = "${system}";
+          allowBroken = true;
+        };
+          mkShell {
+            buildInputs = with python3Packages; [
+              pytest
+              sphinx
+              sphinx_rtd_theme
+              ruff
 
-          );
+              # install gradelib package to 1) make sure it's installable, and
+              # 2) to get its dependencies. But below we'll add it to PYTHONPATH
+              # so we can develop it in place.
+              automata.${system}
+            ];
 
-        defaultPackage = forAllSystems (system:
-            self.automata.${system}
-          );
-      };
+            shellHook = ''
+              export PYTHONPATH="$(pwd)/src/:$PYTHONPATH"
+            '';
+          }
+    );
 
+    defaultPackage = forAllSystems (
+      system:
+        self.automata.${system}
+    );
+  };
 }
